d454eab8e45a966e5abf49e54a9d3ec7caa0fd47
PhKhang <trannguyenphuckhang12@gmail.com>
2023-12-12 08:07:53 +0700

    Add: code for previous features

---
+++ Engine/Board.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <bits/stdc++.h>
+#include "Block.h"
+
+using namespace std;
+
+class Board{
+    private:
+        int rows;
+        int cols;
+        int cellSize;
+        const int HEIGHT = 20;
+        const int WIDTH = 10;
+        Tetriminos* b = new O;
+    public:
+        Board();
+        vector<vector<bool>> board;
+        void createBoard();
+        void print();
+        void draw();
+        bool isCollied(vector<pair<int,int>>);
+        void update();
+};
+
+Board::Board(){
+    rows = 20;
+    cols = 10;
+    cellSize = 30;
+    
+    board = vector<vector<bool>>(20, (vector<bool>(10, 0)));
+    // createBoard();
+
+}
+
+void Board::createBoard(){
+    // for(int row = 0; row < rows; row++){
+    //     for(int col = 0; col < cols; col++){
+    //         board[row][col] = 0;
+    //     }
+    // }
+}
+
+void Board::print(){
+    // for(int row = 0; row < rows; row++){
+    //     for(int col = 0; col < cols; col++){
+    //         cout << board[row][col];
+    //     }
+    //     cout << endl;
+    // }
+}
+
+void Board::draw(){
+    vector<vector<bool>> toDraw(20, vector<bool>(10, 0));
+
+    for(int i = 0; i < 20; i++){
+        for(int j = 0; j < 10; j++)
+            if(board[i][j] == 1)
+                toDraw[i][j] = 1;
+    }
+    
+    vector<pair<int,int>> pos = b->GetAllPoints();
+    for (int i = 0; i < pos.size(); i++) {
+        // cout << pos[i].first << " & " << pos[i].second << endl;
+        toDraw[pos[i].first][pos[i].second] = true;
+    }
+
+    for(int i = 0; i < 20; i++){
+        for(int j = 0; j < 10; j++)
+            cout << toDraw[i][j];
+        cout << endl;
+    }
+}
+
+bool Board::isCollied(vector<pair<int,int>> pos){
+    for (int i = 0; i < pos.size(); i++)
+        if (pos[i].first >= HEIGHT || pos[i].first < 0 || 
+            pos[i].second >= WIDTH || pos[i].second < 0 ||
+            board[pos[i].first][pos[i].second] != 0) {
+                cout << "Cant go\n";
+                return true;
+            }
+            
+    return false;
+}
+
+void Board::update(){
+    int n;
+    cout << "(1) for Left, (3) for Right, (5) to Transform: ";
+    cin >> n;
+    
+    bool result;
+    switch (n) {
+        case 1:
+            cout << "Case 1\n";
+            if (!isCollied(b->GoLeft(true))){
+                b->GoLeft();
+            }
+            break;
+        case 3:
+            cout << "Case 3\n";
+            if (!isCollied(b->GoRight(true))){
+                b->GoRight();
+            }
+            break;
+    }
+    
+    cout << b->y << "+" << b->x << endl;
+    
+}
+
+
---
+++ Engine/main.cpp
@@ -0,0 +1,13 @@
+#include "board.cpp"
+
+int main(){
+    Board board = Board();
+    board.draw();
+    while (1) {
+        board.update();
+        board.draw();
+        cout << endl << endl;
+    }
+    
+    return 0;
+}
