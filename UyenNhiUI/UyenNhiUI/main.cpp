ae89478ed75302846e41bece0c951583dbe74440
nezuni1812 <tranngocuyennhi1812@gmail.com>
2023-12-15 23:04:51 +0700

    Update main.cpp

--- UyenNhiUI/UyenNhiUI/main.cpp
+++ UyenNhiUI/UyenNhiUI/main.cpp
@@ -3,17 +3,6 @@
 #include "Menu.h"
 #include "Player.h"
 
-//int mainh() {
-//    Board board = Board();
-//    board.draw(true);
-//    while (1) {
-//        board.update("right");
-//        // cout << endl << endl;
-//    }
-//
-//    return 0;
-//}
-
 int main()
 {
     RenderWindow window(VideoMode(750, 850), "Tetris", Style::Titlebar | Style::Close);
